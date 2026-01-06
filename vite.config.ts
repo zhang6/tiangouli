eedcb657dc3c78e559ef80f8a83804a415a2ef70
zhang <zam@chuangshi.com>
2026-01-06 20:13:33 +0800

    閰嶇疆 GitHub Pages 閮ㄧ讲锛氭坊鍔?GitHub Actions 宸ヤ綔娴佸拰鏇存柊 Vite 閰嶇疆

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: process.env.NODE_ENV === 'production' ? '/tiangouli/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
